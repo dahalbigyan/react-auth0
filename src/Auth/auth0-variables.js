c5424234521a2d8f762d8b2596cf9dd09a8bcecf
bigyan.dahal@knovva.com <bigyan.dahal@knovva.com>
2018-03-16 10:42:36 -0400

    Changed the clientid.

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'bigyan.auth0.com',
-  clientId: '3QDqqfEAOC6icLE1SGrMHHWo130yms2I',
+  clientId: 'mfgTg9abCTUTvyhSVy0OYXkwSK1QjjMe',
   callbackUrl: 'http://localhost:3000/callback'
 }
