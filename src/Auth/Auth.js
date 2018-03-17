5ea13c31b8bd32098cf3c91cf24834f3530fb4de
bigyan.dahal@knovva.com <bigyan.dahal@knovva.com>
2018-03-16 20:55:10 -0400

    Added authorization functionality.

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,7 +1,6 @@
 import history from '../history';
 import auth0 from 'auth0-js';
 import { AUTH_CONFIG } from './auth0-variables';
-
 export default class Auth {
   auth0 = new auth0.WebAuth({
     domain: AUTH_CONFIG.domain,
